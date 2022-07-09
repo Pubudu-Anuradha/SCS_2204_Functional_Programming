314ce60a0115906f9004ad09c42031d9b2b8a2bc
Pubudu-Anuradha <pubudu.anuradha.satharasinghe@gmail.com>
2022-07-09 18:36:03 +0530

    small correction

--- Tutorial 2/question4a.scala
+++ Tutorial 2/question4a.scala
@@ -1,16 +1,23 @@
-object question4 extends App {
-    val nh = 40;
-    val ot = 30;
-    printf("Take home salary after working %d normal and %d ot hours = Rs. %.2f",nh,ot,takeHomeSalary(nh,ot));
-}
+object question4a extends App {
 
-def normalHoursSalary(hours:Int):Double = hours*250.0;
+  def normalHoursSalary(hours: Int): Double = hours * 250.0;
 
-def otHoursSalary(hours:Int):Double = hours*85.0;
+  def otHoursSalary(hours: Int): Double = hours * 85.0;
 
-def tax(salary:Double):Double = salary*0.12;
+  def tax(salary: Double): Double = salary * 0.12;
 
-def takeHomeSalary(normalHours: Int,otHours:Int) = {
+  def takeHomeSalary(normalHours: Int, otHours: Int) = {
     val salary = (normalHoursSalary(normalHours) + otHoursSalary(otHours));
     salary - tax(salary);
+  }
+
+  val nh = 40;
+  val ot = 30;
+  printf(
+    "Take home salary after working %d normal and %d ot hours = Rs. %.2f",
+    nh,
+    ot,
+    takeHomeSalary(nh, ot)
+  );
+
 }
