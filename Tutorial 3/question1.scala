70256b30911d3a3b540102f5563dbbfbb68187d7
Pubudu-Anuradha <pubudu.anuradha.satharasinghe@gmail.com>
2022-07-15 11:33:06 +0530

    changed formatting

--- Tutorial 3/question1.scala
+++ Tutorial 3/question1.scala
@@ -2,6 +2,7 @@ object question1 extends App {
   def areaOfDisk(radius: Double): Double = {
     math.Pi * radius * radius;
   }
+
   val radius: Double = 5.0;
   printf("Area of disk with radius %.0f is %f\n", radius, areaOfDisk(radius));
 }
